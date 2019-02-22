722804608c73dbfb2483dd7c29f7b1374d7d9fdc
Vidya Kamath <kamathvidya99@gmail.com>
2019-02-22 16:30:04 -0500

    Update AccountRestController.java
    Commit changes

--- src/main/java/com/Account/Account/AccountRestController.java
+++ src/main/java/com/Account/Account/AccountRestController.java
@@ -50,7 +50,7 @@ public class AccountRestController
     public String deleteAccount(@PathVariable("id") String id) {
  
 	    acctInfo.delete(id);
-	   return "Deleted";
+	   return "Deleted the user";
     }
    
 /*    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
